931ccc49ca353a6cd7cf46226de2b3c13b5a3496
satwik <satwik.illini@gmail.com>
2018-06-29 11:14:51 -0500

    day 2

---
+++ src/strings.py
@@ -0,0 +1,17 @@
+rand_string = "   String   "
+
+# Stripping whitespace
+
+rand_string.lstrip()
+rand_string.rstrip()
+rand_string.strip()
+
+text = "text"
+text.capitalize() #Text
+text.upper() #TEXT
+list = ["stuff", "lol", "yylol"]
+" ".join(list) # stuff lol yylol
+
+#count the number of occurrances of a keyword in a string
+
+text.count("keyword")
