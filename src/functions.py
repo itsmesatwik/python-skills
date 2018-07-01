2ff380561e493523bad0b2be9fde5f377247f603
satwik <satwik.illini@gmail.com>
2018-07-01 18:57:44 -0500

    functions

--- src/functions.py
+++ src/functions.py
@@ -3,3 +3,13 @@ def function():
     return 0
 # function end
 
+def change_name(name):
+    name = "lol"
+
+name = "yolo"
+change_name(name)
+# has no effect on the global var name
+
+name = change_name(name)
+#changes name to yolo
+
