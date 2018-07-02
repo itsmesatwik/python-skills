59f6e21d4bcb3b7f68a5fc81ffe4b9c6a17f2662
satwik <satwik.illini@gmail.com>
2018-07-01 19:04:39 -0500

    day 4

---
+++ src/lists.py
@@ -0,0 +1,10 @@
+import math
+import random
+
+#lists in python are dynamic and can hold multiple data types
+
+# creates a list from values 0 to 9
+oneToten = list(range(10))
+
+#strings are lists of characters
+
